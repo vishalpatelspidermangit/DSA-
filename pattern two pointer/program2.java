ebc8bb59c74e272b68a61e0669a51c07b019c126
Vishal Patel <grvishalpatel@gmail.com>
2026-09-08 21:07:26 +0530

    first commit

---
+++ pattern two pointer/program2.java
@@ -0,0 +1,27 @@
+//Given an array arr[] consisting of only 0's and 1's. Modify the array in-place to segregate 0s onto the left side and 1s onto the right side of the array.
+
+public class program2 {
+    void segregate0and1(int[] arr) {
+        int i=0;
+        int j=arr.length-1;
+        while(i>j){
+            if(arr[i]==0){
+                i++;
+            }
+            else if(arr[j]==1){
+                j++;
+
+            }
+            else {
+                arr[i] = 0;
+                arr[j] = 1;
+                i++;
+                j--;
+
+        }
+        
+
+        
+        }
+    }
+}
---
+++ pattern two pointer/program3.java
@@ -0,0 +1,6 @@
+//Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+public class program3 {
+    
+}
