e5186d63a8273c0c29314e51703ccf93015b6d5e
Vishal Patel <grvishalpatel@gmail.com>
2026-09-13 20:18:43 +0530

    soory for late due to sime personal reason

--- pattern 2 pointer/program4.java
+++ pattern 2 pointer/program4.java
@@ -1,3 +1,38 @@
+/*Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.*/
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class program4 {
-    
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate anchors
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int s = nums[left] + nums[right];
+
+                if (s == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (s < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
 }
