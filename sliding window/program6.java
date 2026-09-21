7b70a6432760f78f011446c7ea7d56b7236cd348
Vishal Patel <grvishalpatel@gmail.com>
2026-09-21 22:39:17 +0530

    holy shit

--- sliding window/program6.java
+++ sliding window/program6.java
@@ -14,6 +14,38 @@ Explanation: Replace the two 'A's with two 'B's or vice versa.*/
 public class program6 {
 
     public int characterReplacement(String s, int k) {
-        
+
+        int[] freq = new int[26];
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'A']++;
+
+            maxFreq = Math.max(
+                maxFreq,
+                freq[s.charAt(right) - 'A']
+            );
+
+            int replacements = (right - left + 1) - maxFreq;
+
+            while (replacements > k) {
+
+                freq[s.charAt(left) - 'A']--;
+                left++;
+
+                replacements = (right - left + 1) - maxFreq;
+            }
+
+            maxLength = Math.max(
+                maxLength,
+                right - left + 1
+            );
+        }
+
+        return maxLength;
     }
 }
