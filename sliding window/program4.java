b0eeb815b35d00cf24063eecec733e07bc93fb6b
Vishal Patel <grvishalpatel@gmail.com>
2026-09-21 22:34:39 +0530

    slinding window its not done bro but i an done

--- sliding window/program4.java
+++ sliding window/program4.java
@@ -20,9 +20,33 @@ Input: fruits = [1,2,1]
 Output: 3
 Explanation: We can pick from all 3 trees. */
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class program4 {
     public int totalFruit(int[] fruits) {
-        
+         int n = fruits.length;
+        int left = 0, maxLen = 0;
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int right = 0; right < n; right++) {
+            freq.put(fruits[right], freq.getOrDefault(fruits[right], 0) + 1);
+
+            
+            while (freq.size() > 2) {
+                int leftFruit = fruits[left];
+                freq.put(leftFruit, freq.get(leftFruit) - 1);
+
+                
+                if (freq.get(leftFruit) == 0)
+                    freq.remove(leftFruit);
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+
     }
 }
--- sliding window/program5.java
+++ sliding window/program5.java
@@ -14,10 +14,30 @@ Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers. */
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class program5 {
     public int lengthOfLongestSubstring(String s) {
+         int n = s.length();
+        int left = 0, maxLen = 0;
+        Map<Character, Integer> lastIndex = new HashMap<>();
+
+        for (int right = 0; right < n; right++) {
+            char c = s.charAt(right);
+
+            // if char already seen, move left pointer after its last occurrence
+            if (lastIndex.containsKey(c)) {
+                left = Math.max(left, lastIndex.get(c) + 1);
+            }
+
+            lastIndex.put(c, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
         
     }
-}
+
