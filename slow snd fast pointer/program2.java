d432ad5fb9c9dc29639b642d4e954fa5ab140bd1
Vishal Patel <grvishalpatel@gmail.com>
2026-09-26 21:35:32 +0530

    hand of god

--- slow snd fast pointer/program2.java
+++ slow snd fast pointer/program2.java
@@ -28,6 +28,21 @@ Explanation: There is a cycle in the linked list, where tail connects to the fir
 
 public class program2 {
      public ListNode detectCycle(ListNode head) {
-        
+        ListNode slow = head ;
+        ListNode fast  = head ;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow = head ;
+                while(slow!=fast){
+                    slow = slow.next;
+                    fast = fast.next;
+
+                }return slow;
+            }
+        }
+        return null ;
     }
 }
